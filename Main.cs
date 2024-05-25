1ebe0db2198d11f1c84748bef48189499a516988
privet_so_dna <banoliff@gmail.com>
2024-05-25 14:18:05 +0300

    Сделан рефакторинг кода добавления элементов управления в коллекцию

--- Main.cs
+++ Main.cs
@@ -11,98 +11,101 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using System.CodeDom;
+using Label = System.Windows.Forms.Label;
 
 namespace DiplomaWinForms
 {
     public partial class Main : Form
     {
-        List<List<Control>> fields = new List<List<Control>>();
         Messages msg = new Messages();
         public DataBase db = new DataBase();
         bool isChangingTable;
         string currentTable;
+        List<List<List<Control>>> Tables = new List<List<List<Control>>>();
+        List<List<Control>> Rows = new List<List<Control>>();
+
         public Main()
         {
             InitializeComponent();
         }
 
-        public class Argument
+        private List<List<List<Control>>> pages = new List<List<List<Control>>>();
+
+        class Row
         {
-            public string Name { get; set; }
-            public Type Type { get; set; }
-            public System.Windows.Forms.Label CreateLabel()
+            public List<Control> Add(string Name, Type Type)
             {
-                System.Windows.Forms.Label label = new System.Windows.Forms.Label();
+                List<Control> list = new List<Control>
+                {
+                    CreateLabel(Name),
+                    CreateControl(Name, Type)
+                };
+                return list;
+            }
+            static Label CreateLabel(string Name)
+            {
+                Label label = new Label();
                 label.Name = "label" + Name;
-                label.Text = Name;
                 label.AutoSize = true;
                 label.Anchor = AnchorStyles.Right;
+                if (Name != null)
+                    label.Text = Name;
+                else
+                    label.Text = "null";
                 return label;
             }
-            public Control CreateField()
+
+            static Control CreateControl(string Name, Type Type)
             {
-                Control control = null;
+                Control control = new Control();
                 if (Type == typeof(DateTime))
                     control = new DateTimePicker();
                 else if (Type == typeof(int) || Type == typeof(Int64))
+                {
                     control = new NumericUpDown();
+                    if (control is NumericUpDown numeric)
+                        numeric.Maximum = Int64.MaxValue;
+                }
                 else
                     control = new TextBox();
                 control.Name = "control" + Name;
                 control.Dock = DockStyle.Fill;
+                control.Width = 100;
                 return control;
             }
         }
 
-
         void RefreshUI()
         {
+
             listBoxTables.Items.Clear();
             db.RefreshDS();
+            pages.Clear();
             int maxWidthLabel = 0, maxWidthField = 0;
+
             for (int table = 0; table < db.tablesNames.Rows.Count; table++)
             {
                 listBoxTables.Items.Add(db.tablesNames.Rows[table][0].ToString());
-                List<Control> field = new List<Control>();
-                for (int column = 0; column < db.ds.Tables[table].Columns.Count; column++)
+                List<List<Control>> rows = new List<List<Control>>();
+                foreach (DataColumn column in db.ds.Tables[table].Columns)
                 {
-                    Argument argument = new Argument
+                    Row row = new Row();
+                    rows.Add(row.Add(column.ColumnName, column.DataType));
+                    if (rows.Count >0)
                     {
-                        Name = db.ds.Tables[table].Columns[column].ToString(),
-                        Type = db.ds.Tables[table].Columns[column].DataType
-                    };
-                    System.Windows.Forms.Label label = argument.CreateLabel();
-                    Control control = argument.CreateField();
-                    if (label.Width > maxWidthLabel)
-                        maxWidthLabel = label.Width;
-                    if (control.Width > maxWidthField)
-                        maxWidthField = control.Width;
-                    field.Add(label);
-                    field.Add(control);
-
+                        if (rows[rows.Count - 1][0].Width > maxWidthLabel)
+                            maxWidthLabel = rows[rows.Count - 1][0].Width;
+                        if (rows[rows.Count - 1][1].Width > maxWidthField)
+                            maxWidthField = rows[rows.Count - 1][1].Width;
+                    }
                 }
-                fields.Add(field);
+                pages.Add(rows);
             }
-            TableLayoutPanelLeft.Width = maxWidthLabel + maxWidthField + tableLayoutPanelArguments.Margin.Right;
+            //TableLayoutPanelLeft.Width = maxWidthField + maxWidthLabel;
             if (listBoxTables.Items.Count > 0)
                 listBoxTables.SelectedIndex = 0;
-
-
-            //foreach (List<Control> field in fields)
-            //{
-            //    foreach (Control control in field)
-            //    {
-            //        if (control is Label)
-            //        {
-            //            if (control.Width > maxWidthLabel)
-            //                maxWidthLabel = control.Width;
-            //        } else
-            //            if (control.Width > maxWidthField)
-            //                maxWidthField = control.Width;
-            //    }
-            //}
-
-
         }
 
         private void Main_Load(object sender, EventArgs e)
@@ -114,19 +117,19 @@ namespace DiplomaWinForms
 
         private void listBoxTables_SelectedIndexChanged(object sender, EventArgs e)
         {
-            isChangingTable = true;
-            currentTable = listBoxTables.SelectedItem.ToString();
-            tableLayoutPanelArguments.Controls.Clear();
-            tableLayoutPanelArguments.RowStyles.Clear();
-            tableLayoutPanelArguments.RowCount = 1;
-            tableLayoutPanelArguments.RowCount += db.ds.Tables[currentTable].Columns.Count;
-            for (int i = 0; i < tableLayoutPanelArguments.RowCount; i++)
-                tableLayoutPanelArguments.RowStyles.Add(new RowStyle(SizeType.AutoSize));
-            tableLayoutPanelArguments.Controls.AddRange(fields[listBoxTables.SelectedIndex].ToArray());
-            dataGridViewMain.DataSource = db.ds.Tables[currentTable];
-            isChangingTable = false;
-            Console.WriteLine($"Строк в панели: {tableLayoutPanelArguments.RowCount}\n" +
-                $"Количество полей: {tableLayoutPanelArguments.Controls.Count / 2}");
+            //isChangingTable = true;
+            //currentTable = listBoxTables.SelectedItem.ToString();
+            //tableLayoutPanelArguments.Controls.Clear();
+            //tableLayoutPanelArguments.RowStyles.Clear();
+            //tableLayoutPanelArguments.RowCount = 1;
+            //tableLayoutPanelArguments.RowCount += db.ds.Tables[currentTable].Columns.Count;
+            //for (int i = 0; i < tableLayoutPanelArguments.RowCount; i++)
+            //    tableLayoutPanelArguments.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            //tableLayoutPanelArguments.Controls.AddRange(pages[listBoxTables.SelectedIndex][db.ds.Tables[currentTable].Columns.].ToArray());
+            //dataGridViewMain.DataSource = db.ds.Tables[currentTable];
+            //isChangingTable = false;
+            //Console.WriteLine($"Строк в панели: {tableLayoutPanelArguments.RowCount}\n" +
+            //    $"Количество полей: {tableLayoutPanelArguments.Controls.Count / 2}");
         }
 
         private void buttonAdd_Click(object sender, EventArgs e)
@@ -157,7 +160,7 @@ namespace DiplomaWinForms
             if (!isChangingTable && dataGridViewMain.CurrentRow != null)
                 foreach (DataColumn col in db.ds.Tables[currentTable].Columns)
                     if (dataGridViewMain.CurrentRow.Cells[col.ColumnName].Value != DBNull.Value)
-                        tableLayoutPanelArguments.Controls[col.ColumnName].Text = dataGridViewMain.CurrentRow.Cells[col.ColumnName].Value.ToString();
+                        tableLayoutPanelArguments.Controls["control" + col.ColumnName].Text = dataGridViewMain.CurrentRow.Cells[col.ColumnName].Value.ToString();
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
